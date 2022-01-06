1fab559de087f42071edb5a4eac9a8f88b5bf33d
frozberry <93860959+frozberry@users.noreply.github.com>
2022-01-06 19:01:46 +0000

    Remove stripe server library

--- src/lib/getStripe.ts
+++ src/lib/getStripe.ts
@@ -2,6 +2,7 @@ import { Stripe, loadStripe } from "@stripe/stripe-js"
 
 const stripePublic = process.env.NEXT_PUBLIC_STRIPE_PUB_KEY!
 let stripePromise: Promise<Stripe | null>
+
 const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(stripePublic)
