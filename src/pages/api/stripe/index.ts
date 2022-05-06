349cd1781745f94424f7f3dab9cfe48e06a62481
Henry <henry@henrywu.co.uk>
2022-05-06 12:35:20 +0100

    add all course links

--- src/pages/api/stripe/index.ts
+++ src/pages/api/stripe/index.ts
@@ -1,7 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 
+const courses = {
+  ff: {
+    gbp: "https://buy.stripe.com/9AQaIE0dH5VPctG6oo",
+    india: "https://buy.stripe.com/8wM0406C53NHeBO6or",
+  },
+  pp: {
+    gbp: "https://buy.stripe.com/28obMI4tX1FzfFS6ot",
+    india: "https://buy.stripe.com/eVa6so8Kd4RLdxKeV1",
+  },
+  kotc: {
+    gbp: "https://buy.stripe.com/dR6aIEgcFesl1P26ou",
+    india: "https://buy.stripe.com/4gwdUQ5y1esl1P26ow",
+  },
+}
+
 const GET = async (req: NextApiRequest, res: NextApiResponse) => {
   const country = req.headers["x-vercel-ip-country"] || "none"
+
   const gbp = "https://buy.stripe.com/9AQaIE0dH5VPctG6oo"
   const india = "https://buy.stripe.com/8wM0406C53NHeBO6or"
 
